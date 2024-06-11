5dc630f9bde44c07d7a3d8a9d2919fdc5d8df95f
superfreshdev@gmx.de <superfreshdev@gmx.de>
2024-06-11 16:04:35 +0200

    update

--- js/js-lib.js
+++ js/js-lib.js
@@ -1,44 +1,65 @@
 console.log("### js-lib ### ")
 
 
-const radioSelectMainContent = [
+// const radioSelectMainContent = [
 
-    "radio-books",
-    "radio-language"
+//     "radio-books",
+//     "radio-language"
 
-]
+// ]
 
-const ulMainSelectContent = document.querySelector("#js-nav-select-main-content > label:nth-of-type(1)");
+// const ulMainSelectContent = document.querySelector("#js-nav-select-main-content > label:nth-of-type(1)");
 
 // ulMainSelectContent.style.display = "none";
 
-ulMainSelectContent.addEventListener("click" , ()=> {
+// ulMainSelectContent.addEventListener("click" , ()=> {
 
-    console.log("Bücher")
+//     console.log("Bücher")
    
 
-    const language = document.querySelector("#js-nav-select-main-content > label:nth-of-type(2)");
+//     const language = document.querySelector("#js-nav-select-main-content > label:nth-of-type(2)");
 
-    // console.log( " => " + language.style.position )
-    console.log( "vorher = " + language.style.position )
-    console.log( language )
+//     // console.log( " => " + language.style.position )
+//     console.log( "vorher = " + language.style.position )
+//     console.log( language )
     
-    // if( language.style.position === "absolute" ) {
+//     // if( language.style.position === "absolute" ) {
 
-    //     console.log("relative")
-    //     language.style.position = "relative";
+//     //     console.log("relative")
+//     //     language.style.position = "relative";
         
-    // } else {
+//     // } else {
 
-    //     console.log("absolute")
-    //     language.style.position = "absolute";
+//     //     console.log("absolute")
+//     //     language.style.position = "absolute";
 
-    // }
+//     // }
        
   
-    // console.log( language )
+//     // console.log( language )
 
   
    
     
+// })
+
+
+var imgCafeFullsize = false;
+
+const imgCafe = document.getElementById("imgCafe");
+
+console.log( " cafe = " + imgCafe.getAttribute("src") )
+
+imgCafe.addEventListener( "click" , ()=> {
+
+    console.log("click");
+    imgCafe.classList.toggle("img-header-book-fullsize");
+
+    // if( imgCafeFullsize == false ) {
+
+       
+    // } else {
+
+    // }
+
 })
