8e883a978644a34cf54091c842b57fb8ac93c83e
superfreshdev@gmx.de <superfreshdev@gmx.de>
2024-07-02 20:32:37 +0200

    update

--- js/page/header/header.js
+++ js/page/header/header.js
@@ -10,21 +10,29 @@ const chkNavHeader = document.getElementById('chk-header-nav');
 
 
 
+
 /* ------------------------------------------------------------------- */
 /* Change | Checked/unchecked | slide in/out nav header select
 /* ------------------------------------------------------------------- */   
 
 chkNavHeader.addEventListener( 'change' , ()=> {
 
-    // console.log( " clientHeight = " + valueHeightNavHeaderSelect + "px")
+    console.log( " clientHeight = " + valueHeightNavHeaderSelect + "px")
 
     if( chkNavHeader.checked == true ) {
 
+       
         mainBody.style.transform = "translateY(0%)"
+
+        // navHeaderSelect.style.backgroundColor = "brown";
+
+       
 
     } else {
 
         mainBody.style.transform = `translateY(${ valueHeightNavHeaderSelect }px)`;
+       
+       
     }
 
  
--- js/windowsLoad/windowsLoad.js
+++ js/windowsLoad/windowsLoad.js
@@ -16,9 +16,11 @@ var valueHeightNavHeaderSelect = 0;
 var fixRowGapBodyContainer = 14;
 valueHeightNavHeaderSelect = ( valueHeightNavHeaderSelect - fixRowGapBodyContainer );
 
+
 /* ---------------------------------------------- */
 /* ...
 /* ---------------------------------------------- */
+
 
 window.addEventListener( 'load' , ()=> {
 
@@ -26,8 +28,25 @@ window.addEventListener( 'load' , ()=> {
     /* ------------------------------------------------------ */
     /* Set Height | Nav Header Select
     /* ------------------------------------------------------ */
+
+    console.log( " windows Load = [navHeaderSelect | Height ] = " + valueHeightNavHeaderSelect )
+
     valueHeightNavHeaderSelect-=getHeightOfIdElement( navHeaderSelect )
+
+    console.log( " windows Load = [navHeaderSelect | Height ] = " + valueHeightNavHeaderSelect )
+
     mainBody.style.transform = `translateY(${ valueHeightNavHeaderSelect }px)`;
 
+   
+    // delte 14px of row gap of body-container , we dont need it 
+
+    // valueHeightNavHeaderSelect = Math.abs( (valueHeightNavHeaderSelect + 14) );
+    // console.log( " windows Load = [navHeaderSelect | Height ] = " + valueHeightNavHeaderSelect )
+
+    // must be calced and test it the px value
+    // calced +1px ( browser bug calc )
+
+    // mainBody.style.height = `calc(100% + 169px)`;
+  
     
 })
